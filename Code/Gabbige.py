72f7fbbd4966c07ef29740ba0c23e53d1392d222
HarveyCR <lbf.asus.k00z.2014@gmail.com>
2022-01-15 19:13:40 +0300

    Итороавя часть
    Готовая часть проекта, маусимум возможны дизайновые измемения.

--- Code/Gabbige.py
+++ Code/Gabbige.py
@@ -1,6 +1,230 @@
 import sys
 import pygame
 import os
+import rand_word
+import random
+
+
+class Game_play:
+    def __init__(self, settings):
+        self.letter_is = []
+        self.letter_not = []
+
+        lang = settings[0]
+
+        # Часть речи
+        sp = ''
+        self.three = settings[2]
+        if self.three == (1, 0):
+            sp = "n"
+        elif self.three == (0, 1):
+            sp = "v"
+        elif self.three == (1, 1):
+            sp = "nv"
+
+        # Сложность
+        dif = -1
+        self.two = settings[1]
+        if self.two == "easy":
+            dif = 0
+        elif self.two == "hard":
+            dif = 1
+        else:
+            dif = 2
+
+        self.wordr = rand_word.get_word(lang, dif, sp).lower()
+        self.word = list(self.wordr)
+
+        self.word_star = ["*" for i in range(len(self.wordr))]
+
+        print(self.wordr)
+        self.Game_start()
+
+    def get_word(language, difficulty, p_speech):
+        if language == "rus":
+            f_name = 'rus'
+        elif language == "eng":
+            f_name = 'eng'
+        else:
+            print('ERROR: get_word(): Wrong "language=="', language)
+            return ""
+
+        p_sp = []
+        if "n" in p_speech:
+            p_sp.append("_n")
+        elif "v" in p_speech:
+            p_sp.append("_v")
+
+        if len(p_sp) == 0:
+            print('ERROR: get_word(): Wrong "p_speech=="', p_speech)
+            return ""
+
+        if difficulty == 0:
+            a = 1
+            b = 4
+        elif difficulty == 1:
+            a = 5
+            b = 8
+        elif difficulty == 2:
+            a = 9
+            b = 99
+        else:
+            print('ERROR: get_word(): Wrong "difficulty=="', difficulty)
+            return ""
+
+        a += len("\n")
+        b += len("\n")
+        words = []
+        for v in p_sp:
+            f = f_name + v + ".txt"
+            with open(f) as file_txt:
+                w = [s.strip("\n") for s in file_txt if a <= len(s) <= b]
+                words.extend(w)
+                w = []
+        # print(len(words))
+        rez = random.choice(words)
+
+        words = []
+        return rez
+
+    def Game_start(self):
+        background = pygame.transform.scale(load_image(f"Game_0.png"), (width, height))
+        screen.blit(background, (0, 0))
+
+        font = pygame.font.Font(None, 50)
+        text_star = font.render(''.join(self.word_star), True, (0, 0, 0))
+        screen.blit(text_star, (492, 300))
+
+        k = 0
+        self.used_word = []
+        self.not_used_word = []
+
+        running = True
+        letter_is = False
+        word_is = False
+        msg_letter = ''
+        msg_word = ''
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x = event.pos[0]
+                    y = event.pos[1]
+                    # print(x, y)
+
+                    if 537 < x < 1072 and 506 < y < 541 and msg_letter != '':
+                        if msg_letter in self.used_word:
+                            text_stick = font.render(str("Введите не использованную букву"), True, (0, 0, 0))
+                            screen.blit(text_stick, (485, 368))
+                        else:
+                            err, k = self.check_letter(msg_letter, k)
+
+                    elif 537 < x < 1072 and 506 < y < 541 and msg_letter == '':
+                        print('Введите букву')
+
+                    elif 485 < x < 1200 and 435 < y < 490:
+                        pygame.draw.rect(screen, "white", [(485, 435), (100, 50)], width=0)
+                        text_stick = font.render('|', True, (0, 0, 0))
+                        screen.blit(text_stick, (485, 450))
+                        letter_is = True
+                        msg_letter = ''
+
+                    elif letter_is:
+                        pygame.draw.rect(screen, "white", [(485, 435), (100, 50)], width=0)
+                        msg_letter = ''
+                        letter_is = False
+
+                    if 573 < x < 1072 and 675 < y < 705 and msg_word != '':
+                        k = self.check_word(msg_word, k)
+
+                    elif 573 < x < 1072 and 675 < y < 705 and msg_word == '':
+                        print('Введите слово')
+
+                    elif 485 < x < 1198 and 601 < y < 657:
+                        pygame.draw.rect(screen, "white", [(488, 604), (900, 53)], width=0)
+                        text_stick = font.render('|', True, (0, 0, 0))
+                        screen.blit(text_stick, (490, 620))
+                        word_is = True
+                        msg_word = ''
+
+                    elif word_is:
+                        pygame.draw.rect(screen, "white", [(488, 604), (900, 53)], width=0)
+                        msg_word = ''
+                        word_is = False
+
+                if event.type == pygame.KEYDOWN:
+                    if letter_is:
+                        msg_letter = event.unicode
+                        # print(msg_letter)
+                        pygame.draw.rect(screen, "white", [(485, 435), (900, 53)], width=0)
+                        text_stick = font.render(msg_letter, True, (0, 0, 0))
+                        screen.blit(text_stick, (485, 450))
+
+                    elif word_is:
+                        msg_word += event.unicode
+                        # print(msg_word)
+                        pygame.draw.rect(screen, "white", [(488, 604), (100, 50)], width=0)
+                        text_stick = font.render(msg_word, True, (0, 0, 0))
+                        screen.blit(text_stick, (490, 620))
+
+            pygame.display.flip()
+            clock.tick(fps)
+    pygame.quit()
+
+    def check_letter(self, letter_is, k):
+        letter = letter_is.lower()
+        if k < 10:
+            if letter == '' or letter.isalpha() is False:
+                er = "Введите букву"
+            elif letter in self.word:
+                er = "Есть такая буква!"
+                self.used_word.append(letter)
+                r = 0
+                for l in self.word:
+                    if l == letter:
+                        self.word_star[r] = l
+                    r += 1
+            elif letter not in self.word:
+                er = "Такой буквы нет"
+                self.not_used_word.append(letter)
+                k += 1
+            # print(letter)
+
+            font = pygame.font.Font(None, 50)
+            background = pygame.transform.scale(load_image(f"Game_{k}.png"), (width, height))
+            screen.blit(background, (0, 0))
+            stars = font.render(''.join(self.word_star), True, (0, 0, 0))
+            screen.blit(stars, (492, 300))
+            text_stick = font.render(str(er), True, (0, 0, 0))
+            screen.blit(text_stick, (485, 368))
+
+            text_stick = font.render(', '.join(self.used_word), True, (0, 0, 0))
+            screen.blit(text_stick, (490, 70))
+            text_stick = font.render(', '.join(self.not_used_word), True, (0, 0, 0))
+            screen.blit(text_stick, (490, 176))
+            return er, k
+        else:
+            Lost()
+            pygame.quit()
+
+    def check_word(self, word, k):
+        name = word.lower()
+        if name == self.wordr:
+            Win()
+        else:
+            k += 1
+            font = pygame.font.Font(None, 50)
+            background = pygame.transform.scale(load_image(f"Game_{k}.png"), (width, height))
+            screen.blit(background, (0, 0))
+            text_stick = font.render(str("Неправильное слово"), True, (0, 0, 0))
+            screen.blit(text_stick, (485, 368))
+            if k == 10:
+                Lost()
+            else:
+                return k
 
 
 def load_image(name):
@@ -32,7 +256,7 @@ def start_screen():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                # print(event.pos)
+                # print("Start")
                 if 545 < event.pos[0] < 655 and 408 < event.pos[1] < 426:
                     rules()
                 if 455 < event.pos[0] < 640 and 274 < event.pos[1] < 380:
@@ -40,6 +264,7 @@ def start_screen():
 
         pygame.display.flip()
         clock.tick(fps)
+    pygame.quit()
 
 
 def rules():
@@ -64,11 +289,13 @@ def rules():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                # print(event.pos)
+                # print("Rules")
                 if 14 < event.pos[0] < 77 and 678 < event.pos[1] < 698:
                     start_screen()
+                    running = False
         pygame.display.flip()
         clock.tick(fps)
+    pygame.quit()
 
 
 def Leng():
@@ -92,6 +319,7 @@ def Leng():
 
         pygame.display.flip()
         clock.tick(fps)
+    pygame.quit()
 
 
 def Difficulty():
@@ -109,14 +337,18 @@ def Difficulty():
             if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 # print(event.pos)
                 if 74 < event.pos[0] < 380 and 233 < event.pos[1] < 360:
+                    running = False
                     return "easy"
                 elif 475 < event.pos[0] < 775 and 242 < event.pos[1] < 360:
+                    running = False
                     return "normal"
                 elif 840 < event.pos[0] < 1100 and 236 < event.pos[1] < 360:
+                    running = False
                     return 'hard'
 
         pygame.display.flip()
         clock.tick(fps)
+    pygame.quit()
 
 
 def Speech_part(f, s, e):
@@ -125,7 +357,6 @@ def Speech_part(f, s, e):
 
     running = True
     while running:
-        # screen.fill((255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -133,11 +364,17 @@ def Speech_part(f, s, e):
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # print(event.pos)
                 if 61 < event.pos[0] < 124 and 130 < event.pos[1] < 190:
-                    f = 1
+                    if f == 1:
+                        f = 0
+                    else:
+                        f = 1
                     background = pygame.transform.scale(load_image(f"ps{f}{s}{e}.png"), (width, height))
                     screen.blit(background, (0, 0))
                 elif 780 < event.pos[0] < 865 and 125 < event.pos[1] < 190:
-                    s = 1
+                    if s == 1:
+                        s = 0
+                    else:
+                        s = 1
                     background = pygame.transform.scale(load_image(f"ps{f}{s}{e}.png"), (width, height))
                     screen.blit(background, (0, 0))
                 if 435 < event.pos[0] < 760 and 280 < event.pos[1] < 400:
@@ -147,14 +384,17 @@ def Speech_part(f, s, e):
                         screen.blit(background, (0, 0))
                         e = 0
                     else:
+                        running = False
                         return f, s
+
 
         pygame.display.flip()
         clock.tick(fps)
+    pygame.quit()
 
 
-def show_settings():
-    background = pygame.transform.scale(load_image("Show.png"), (width, height))
+def Win():
+    background = pygame.transform.scale(load_image("Win.png"), (width, height))
     screen.blit(background, (0, 0))
 
     running = True
@@ -166,13 +406,88 @@ def show_settings():
                 sys.exit()
             if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 # print(event.pos)
-                if 545 < event.pos[0] < 655 and 408 < event.pos[1] < 426:
-                    rules()
-                if 455 < event.pos[0] < 640 and 274 < event.pos[1] < 380:
-                    return
+                if 9 < event.pos[0] < 760 and 615 < event.pos[1] < 710:
+                    start()
+                if 782 < event.pos[0] < 1187 and 616 < event.pos[1] < 707:
+                    exit()
 
         pygame.display.flip()
         clock.tick(fps)
+    pygame.quit()
+
+def Lost():
+    background = pygame.transform.scale(load_image("Lost.png"), (width, height))
+    screen.blit(background, (0, 0))
+
+    running = True
+    while running:
+        # screen.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                if 610 < event.pos[0] < 1150 and 660 < event.pos[1] < 700:
+                    start()
+                if 415 < event.pos[0] < 785 and 490 < event.pos[1] < 580:
+                    exit()
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
+
+def show_settings(settings):
+    background = pygame.transform.scale(load_image("Show.png"), (width, height))
+    screen.blit(background, (0, 0))
+    x, y = 518, 165
+    rule = []
+    for i in settings:
+        if i == 'rus':
+            leng = "Русский"
+        elif i == 'eng':
+            leng = "Английский"
+        if i == "easy":
+            dif = 'Легко'
+        elif i == "normal":
+            dif = 'Среднее'
+        elif i == "hard":
+            dif = "сложно"
+        elif i == (1, 0):
+            part = 'Существительные'
+        elif i == (0, 1):
+            part = 'Глагол'
+        elif i == (1, 1):
+            part = 'Существительные и Глагол'
+    rule.append(leng)
+    rule.append(dif)
+    rule.append(part)
+
+    for _ in rule:
+        font = pygame.font.Font(None, 50)
+        text = font.render(str(_), True, (0, 0, 0))
+        screen.blit(text, (x, y))
+        y += 100
+    running = True
+    while running:
+        # screen.fill((255, 255, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                if 610 < event.pos[0] < 1150 and 660 < event.pos[1] < 700:
+                    settings = [Leng(), Difficulty(), Speech_part(0, 0, 0)]
+                    show_settings(settings)
+                if 415 < event.pos[0] < 785 and 490 < event.pos[1] < 580:
+                    Game_play(settings)
+
+        pygame.display.flip()
+        clock.tick(fps)
+    pygame.quit()
+
+def start():
+    settings = [Leng(), Difficulty(), Speech_part(0, 0, 0)]
+    show_settings(settings)
+    return
 
 
 pygame.init()
@@ -187,8 +502,7 @@ clock = pygame.time.Clock()
 
 start_screen()
 settings = [Leng(), Difficulty(), Speech_part(0, 0, 0)]
-
-show_settings()
+show_settings(settings)
 
 while running:
     screen.fill((255, 255, 255))
@@ -202,3 +516,6 @@ while running:
     clock.tick(fps)
 
 pygame.quit()
+
+if __name__ == '__main__':
+    print("")
